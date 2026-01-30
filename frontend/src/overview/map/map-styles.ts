9b0d463a5aa973191b7871307779fa0b7ba36baa
Aaron Studer <aaron.studer@ergon.ch>
2026-01-30 17:43:32 +0100

    improve contrast in dark map

--- frontend/src/overview/map/map-styles.ts
+++ frontend/src/overview/map/map-styles.ts
@@ -2,18 +2,18 @@
 export const lightMapStyle = [];
 
 export const darkMapStyle = [
-    {elementType: "geometry", stylers: [{color: "#1d1f23"}]},
-    {elementType: "labels.text.fill", stylers: [{color: "#8a8a8a"}]},
-    {elementType: "labels.text.stroke", stylers: [{color: "#1d1f23"}]},
+    {elementType: "geometry", stylers: [{color: "#111111"}]},
+    {elementType: "labels.text.fill", stylers: [{color: "#c8c7c7"}]},
+    {elementType: "labels.text.stroke", stylers: [{color: "#3c4049"}]},
 
     {
         featureType: "water",
         elementType: "geometry",
-        stylers: [{color: "#0f252e"}],
+        stylers: [{color: "#4097bc"}],
     },
     {
         featureType: "road",
         elementType: "geometry",
-        stylers: [{color: "#2c2f36"}],
+        stylers: [{color: "#626978"}],
     },
 ];
